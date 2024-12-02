d87cffbae1e74e063e79e9d1f91bfcbb194599a4
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-02 09:36:47 +0300

    First Trial

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Book:
-    pass
     
+    def __init__(self, title="New Book", page_count = 0):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        """Get Book Title"""
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        """"Set Book Title"""
+        self._title = title
+
+    @property
+    def page_count(self):
+        """Get Book Page Count"""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        """Set Book Page Count"""
+        if type(page_count) == type(int()):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand="Ngucci", size=1000000):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand(self):
+        """Get the Brand Name"""
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+
+    @property
+    def size(self):
+        """Get the Shoe Size"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+    
+    @property
+    def condition(self):
+        """Get the shoe condition"""
+        return self._condition
+    
+    @condition.setter
+    def condition(self, condition):
+        self._condition = condition
